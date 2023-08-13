3a803e328493b5841300da91e8fd3009e6d1d4b6
T3Nero <ryan-16@hotmail.co.uk>
2023-08-13 18:53:39 +0100

    Initial commit

---
+++ Source/LoneRanger/LoneRangerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoneRangerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LONERANGER_API ALoneRangerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
